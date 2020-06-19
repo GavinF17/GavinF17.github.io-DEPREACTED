2a5f2061d232d8657301a2f5e8f84b3684e675f9
Gavin Fenton <gavin_fenton@msn.com>
2020-06-19 17:38:40 +0100

    Testing React build in root

---
+++ precache-manifest.92eed804a89c0ccd867a13828f2afc42.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d39d0199a79d8e0394ee51a7c2e4172",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6750d2a7b389456c6796",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a804d402f5921c836455",
+    "url": "/static/js/2.1b103b41.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1b103b41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6750d2a7b389456c6796",
+    "url": "/static/js/main.84998b30.chunk.js"
+  },
+  {
+    "revision": "32ba96c7268e37cadc3e",
+    "url": "/static/js/runtime-main.219c3ace.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
